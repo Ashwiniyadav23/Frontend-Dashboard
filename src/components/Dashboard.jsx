df4848c06896899e332c9c899324c3af9e6f5769
Ashwini Yadav <ashwiniyadav23@navgurukul.org>
2025-01-29 17:44:39 +0530

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
-import axios from "axios";
+import axios from 'axios';
 import {
   BarChart,
   Bar,
